aa40e1e116c2c7043e18864cc1e82a1f1953bca0
GRB24er <GRB24er@users.noreply.github.com>
2026-03-14 08:57:13 -0700

    Add Tawk.to live chat widget

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,21 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
           <main>{children}</main>
           <Toaster />
         </SessionProvider>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/69b584e896a7f41c3840d9b6/1jjmgubch';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   );
